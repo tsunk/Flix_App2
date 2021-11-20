84fe2df83b8b25c3593701eaaf9fe2db26a2d38f
tsunk <56943141+tsunk@users.noreply.github.com>
2021-11-19 23:35:59 -0500

    commit

--- Flix_App2/ViewController.swift
+++ Flix_App2/ViewController.swift
@@ -6,12 +6,67 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        cell.textLabel!.text = "row: \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"]
+        cell.titleLabel.text = title as? String
+
+        let synopsis = movie["overview"] as? String
+        cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
+
+    var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        print("hello")
+        tableView.rowHeight = 150
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    print("got data")
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
+                    print("got movies")
+                    print(self.movies)
+                    // TODO: Reload your table view data
+                    self.tableView.reloadData()
+                    print("reloaded table")
+             }
+        }
+        task.resume()
     }
 
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flix_App2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flix_App2
+  pod "AlamofireImage"
+
+end
